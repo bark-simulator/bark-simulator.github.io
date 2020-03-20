57d6f9471b4ca261404cd35fb1f55ae82d481e8d
Patrick <patrickhart.1990@gmail.com>
2020-03-20 12:30:08 +0100

    Updates

--- component---src-pages-documentation-js-926fdd40f56a2ed75a87.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{mnx0:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-documentation-js-926fdd40f56a2ed75a87.js.map
