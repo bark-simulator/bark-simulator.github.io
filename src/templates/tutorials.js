45cfb52ed661339a5a2b1caf2e1bc57ec41cf2da
patrickhart <patrickhart.1990@gmail.com>
2020-05-30 22:56:30 +0200

    inactive links

--- src/templates/tutorials.js
+++ src/templates/tutorials.js
@@ -14,13 +14,13 @@ export default function BlogPost({ data }) {
         <p class="mb-2 text-gray-500 uppercase tracking-wide font-bold text-sm">BARK</p>
         <Link to="/tutorials/" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" activeClassName="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Installation</Link>
         <Link to="/tutorials/first_example/" partiallyActive={true} class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" activeClassName="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">First Example</Link>
-        {/* <a href="#" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" classActive="font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Diving Deeper</a>
-        <a href="#" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" classActive="font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Advanced Topics</a>
-        <p class="mb-2 text-gray-500 uppercase tracking-wide font-bold text-sm mt-8">BARK-ML</p>
-        <a href="#" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" classActive="font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Installation</a>
-        <a href="#" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" classActive="font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Getting Started</a>
-        <a href="#" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" classActive="font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Environments</a>
-        <a href="#" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" classActive="font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Training an Agent</a> */}
+        <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Diving Deeper</a>
+        <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Advanced Topics</a>
+        <p class="text-gray-400 mb-2 uppercase tracking-wide font-bold text-sm mt-8">BARK-ML</p>
+        <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Installation</a>
+        <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Getting Started</a>
+        <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Environments</a>
+        <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Training an Agent</a>
       </div>
       <div class="w-5/6 pl-12">
             <h1 class="text-4xl text-gray-800">{post.frontmatter.title}</h1>
