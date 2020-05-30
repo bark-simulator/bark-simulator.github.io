69035431ee5866dca1f25a81711f59d543b08a0a
patrickhart <patrickhart.1990@gmail.com>
2020-05-30 11:28:57 +0200

    Updates

--- static/fa20a811bc9ad972dff0e6ef3c94fb02/people.js
+++
@@ -1,7 +0,0 @@
-[
-  {
-    "name": "Patrick Hart",
-    "description": "Developer",
-    "image": "bark_logo.jpg"
-  }
-]
--- styles-56d7fc8c61aa7b00251b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{C3Uj:function(n,w,o){}}]);
-//# sourceMappingURL=styles-56d7fc8c61aa7b00251b.js.map
