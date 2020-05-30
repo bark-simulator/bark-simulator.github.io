66721fafc1dd45566aaa8a02af5d705507c2a416
patrickhart <patrickhart.1990@gmail.com>
2020-05-30 23:10:48 +0200

    optimize for mobile

--- src/templates/tutorials.js
+++ src/templates/tutorials.js
@@ -10,7 +10,7 @@ export default function BlogPost({ data }) {
     <Layout>
     <SEO title="Tutorials" />
     <div class="flex flex-wrap">
-      <div class="w-1/6">
+      <div class="w-full md:w-1/6">
         <p class="mb-2 text-gray-500 uppercase tracking-wide font-bold text-sm">BARK</p>
         <Link to="/tutorials/" class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" activeClassName="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">Installation</Link>
         <Link to="/tutorials/first_example/" partiallyActive={true} class="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block" activeClassName="px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block font-medium rounded absolute inset-0 bg-blue-200 text-blue-600">First Example</Link>
@@ -22,9 +22,9 @@ export default function BlogPost({ data }) {
         <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Environments</a>
         <a href="#" class="text-gray-400 px-2 -mx-2 py-1 transition duration-200 ease-in-out relative block">Training an Agent</a>
       </div>
-      <div class="w-5/6 pl-12">
+      <div class="w-full md:w-5/6 md:pl-12">
             <h1 class="text-4xl text-gray-800">{post.frontmatter.title}</h1>
-            <div class="w-3/4" dangerouslySetInnerHTML={{ __html: post.html }} />
+            <div class="w-full md:w-3/4" dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </div>
   </Layout>
