abd1f845e8e73eaa6d0213cbc4a6e54231d03b54
patrickhart <patrickhart.1990@gmail.com>
2020-06-25 00:30:09 +0200

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,10 @@ const IndexPage = ({data}) => {
 
   return (<Layout>
     <SEO title="Home" />
-      <div class="flex flex-wrap md:justify-center mt-4">
-        <div class="w-full flex-wrap p-4 px-4 md:w-1/2 md:p-12 md:px-0 md:pr-48">
-          <h1 class="text-4xl text-gray-800 font-normal mb-1">Centered on behavior models.</h1>
-          <p class="tracking-wide text-lg leading-relaxed">
+      <div class="flex flex-wrap md:justify-center mt-4 content-center">
+        <div class="w-full flex-wrap px-4 md:w-1/2 md:p-12 lg:px-24">
+          <h1 class="text-4xl text-gray-800 font-normal mb-1 text-center">Centered on behavior models.</h1>
+          <p class="tracking-wide text-xl mt-4 leading-relaxed">
             BARK offers a behavior model-centric simulation framework that enables fast-prototyping and the development of behavior models.
             Behavior models can easily be integrated &mdash; either using Python or C++.
             Various behavior models are available ranging from machine learning to conventional approaches.
@@ -24,18 +24,18 @@ const IndexPage = ({data}) => {
           <Img objectFit="cover-object" fluid={data.barkScreenshot.childImageSharp.fluid} />     
         </div>
       </div>
-      <div class="flex flex-wrap md:justify-center mt-8">
+      <div class="flex flex-wrap md:justify-center mb-12">
         <div class="w-full md:w-1/2">
           <img class="object-cover w-full" src={BarkMerging} />    
         </div>
-        <div class="w-full p-4 px-4 md:w-1/2 md:p-12 md:px-0 md:pl-48">
-          <h1 class="text-4xl text-gray-800 font-normal mb-1">Benchmarking interactive behaviors.</h1>
-          <p class="tracking-wide text-lg leading-relaxed">
+        <div class="w-full px-4 md:w-1/2 md:p-12 lg:px-16">
+          <h1 class="text-4xl text-gray-800 font-normal mb-1 text-center">Benchmarking interactive behaviors.</h1>
+          <p class="tracking-wide text-xl leading-relaxed mt-4">
             Going along with prototyping and developing behavior models is to benchmark their performance.
             BARK provides a benchmark suite that tracks the performance of behavior models &mdash; also during development.
             Unlike many simulation frameworks, BARK offers benchmarking using fully interactive behaviors.
           </p>
-          <div class="flex justify-left py-2 mt-6">
+          <div class="flex justify-center py-2 mt-8">
           <Link to="/tutorials/" style={{background: 'linear-gradient(190deg,#2983FB,#1353B9)'}} class="block shadow-2xl transition duration-500 ease-in-out border text-white hover:shadow-lg font-semibold py-4 px-6 rounded-lg ">
             Get Started &#xbb;
           </Link>
