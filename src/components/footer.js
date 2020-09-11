8b1e0b1ce0e868228ef5180d9025514422c80614
patrickhart <patrickhart.1990@gmail.com>
2020-09-11 11:40:37 +0200

    change copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,7 @@ const Footer = ({}) => (
   <footer class="flex items-center justify-center text-gray-200" style={{padding: `100px 0% 0% 0%`}} >
     <div class="flex items-center justify-between flex-wrap py-8 px-6 pt-8" style={{width: `100%`, backgroundColor: `rgb(3, 70, 147)`}}>
         <div class="w-1/2 text-sm">
-          © {new Date().getFullYear()} Julian Bernhard, Klemens Esterle, Patrick Hart and Tobias Kessler
+          © {new Date().getFullYear()} fortiss GmbH
         </div>
         <div class="w-1/2 text-right text-sm">
           Supported by: <a class="hover:text-blue-500" href="https://www.fortiss.org/">fortiss</a>, <a class="hover:text-blue-500" href="https://aid-driving.eu/">AID</a>
