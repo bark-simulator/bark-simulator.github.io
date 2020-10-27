a717546c745b6278acdd71adf1708285a6ba916e
patrickhart <patrickhart.1990@gmail.com>
2020-10-27 14:38:15 +0100

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,15 +5,14 @@ import Image from "../components/image"
 import "../styles/site.css"
 
 const Footer = ({}) => (
-  <div className="mt-12">
-  <div className="skewed-divider"></div>
+  <div className="mt-6">
   <footer class="flex items-center justify-center text-gray-200" style={{padding: `100px 0% 0% 0%`}} >
     <div class="flex items-center justify-between flex-wrap py-8 px-6 pt-8" style={{width: `100%`, backgroundColor: `rgb(3, 70, 147)`}}>
         <div class="w-1/2 text-sm">
-          © {new Date().getFullYear()} fortiss GmbH
+          © {new Date().getFullYear()} <a class="hover:text-blue-500" href="https://www.fortiss.org/">fortiss GmbH</a>
         </div>
         <div class="w-1/2 text-right text-sm">
-          Supported by: <a class="hover:text-blue-500" href="https://www.fortiss.org/">fortiss</a>, <a class="hover:text-blue-500" href="https://aid-driving.eu/">AID</a>
+          {/* Supported by: <a class="hover:text-blue-500" href="https://www.fortiss.org/">fortiss</a> */}
         </div>
     </div>
   </footer>
