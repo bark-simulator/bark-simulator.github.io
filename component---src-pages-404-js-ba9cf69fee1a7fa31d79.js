237c246017ffd0caf5b24a31ddab14237fd7d616
patrickhart <patrickhart.1990@gmail.com>
2021-05-25 22:34:18 +0200

    Updates

---
+++ component---src-pages-404-js-ba9cf69fee1a7fa31d79.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Wbzz"),c=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"This page does not exist."),l.a.createElement("p",null,"Go back to the ",l.a.createElement(o.Link,{to:"/"},"main page"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ba9cf69fee1a7fa31d79.js.map
